b4a34f03e65834744994f559f356f49f7047c00a
dennisysj <dennisysj@gmail.com>
2025-09-16 14:59:01 -0700

    adding the calculator problem, ready to be cloned

---
+++ 6calculator.py
@@ -0,0 +1,17 @@
+#Make a calculator for 2 variables
+# "*" is multiply and "/" is divide so 2*2 = 4, 10/5 = 2.
+userInput = input("What would you like to do? \n Choose one option: + - / * :")
+num1 = 0
+num2 = 0
+
+#addition condition block
+
+
+#subtraction condition block
+
+
+#division condition block
+
+
+
+#multiplication
