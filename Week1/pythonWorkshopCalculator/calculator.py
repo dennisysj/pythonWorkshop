84cba1aa1d2ee796bd87277c72ba89acdc9184d5
dennisysj <dennisysj@gmail.com>
2025-09-30 15:25:32 -0700

    wk1

--- Week1/pythonWorkshopCalculator/calculator.py
+++ Week1/pythonWorkshopCalculator/calculator.py
@@ -1,5 +1,6 @@
 #Make a calculator for 2 variables
 # "*" is multiply and "/" is divide so 2*2 = 4, 10/5 = 2.
+#also this is a comment btw, it won't affect your code.
 userInput = input("What would you like to do? \n Choose one option: + - / * :")
 num1 = 1
 num2 = 2
@@ -13,7 +14,9 @@ elif(userInput =="-"): #remember elif stands for else if
     print(num1-num2)
 
 #division condition block
-
-
+elif(userInput =="/"):
+    print(num1/num2)
 
 #multiplication
+elif(userInput=="*"):
+    print(num1*num2)
