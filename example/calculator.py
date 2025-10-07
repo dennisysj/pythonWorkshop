791e922fd1a274cbca168597454694b84fa08da6
dennisysj <dennisysj@gmail.com>
2025-10-07 14:29:47 -0700

    project file

---
+++ example/calculator.py
@@ -0,0 +1,21 @@
+#follow the slides to complete the project!
+#Make sure to commit your changes to github so you don't lose your progress.
+#Make a calculator for 2 variables
+# "*" is multiply and "/" is divide so 2*2 = 4, 10/5 = 2.
+userInput = input("What would you like to do? \n Choose one option: + - / * :")
+num1 = 1
+num2 = 2
+
+#addition condition block.
+
+
+#subtraction condition block
+
+
+#division condition block
+
+
+#multiplication
+
+
+
