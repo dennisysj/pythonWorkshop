377584002fad4b820209c2b2815a98d5b03ea0f5
dennisysj <dennisysj@gmail.com>
2025-10-27 22:47:38 -0700

    week6 dictionaries

--- Week6/dictionaries.py
+++ Week6/dictionaries.py
@@ -1,6 +1,24 @@
 student = {
-    "name": "Dennis",
+    "name": "Jacob",
     "age": 21,
     "major": "Computer Science"
 }
+print(student) #will just print out each key-value pair
+print(student["name"])
+print(student["major"])
+#try printing out his age!
+
+
+student["studentID"] = "J1234"  # Adds a new key-value pair
+print(student["studentID"])
+
+
+#Jacob's birthday happened today so we have to update his age!
+student["age"] = 22   # Updates existing value
+
+
+#his CGPA is 3.8 add it to the dictionary, and print it!
+
+
+
 print(student)
